72adb6e137e30f1c27b22282b3d35484471ea722
gmnz <gmnzmailfake@gmail.com>
2026-07-25 01:22:45 +0200

    quick fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/brand/banner.jpg",
+        url: "/brand/banner-v2.png",
         width: 1200,
         height: 630,
         alt: "Medecine Hub - Annales et QCM gratuits de médecine",
